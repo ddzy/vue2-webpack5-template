bb6489efe149abe075ffcce272ce95a739f615cf
ddzy <1766083035@qq.com>
2020-12-18 11:21:15 +0800

    feat: config webpack-dev-server

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const Webpack = require('webpack');
 
 module.exports = {
   entry: './src/main.js',
@@ -32,5 +33,16 @@ module.exports = {
     new HtmlWebpackPlugin({
       title: 'vue2-webpack5-template',
     }),
+    new Webpack.HotModuleReplacementPlugin(),
   ],
+  devtool: process.env.NODE_ENV === 'development' ? 'eval-source-map' : 'source-map',
+  devServer: {
+    contentBase: path.resolve(__dirname, 'dist'),
+    open: true,
+    port: 8888,
+    compress: true,
+    hot: true,
+    clientLogLevel: 'silent',
+    noInfo: true,
+  },
 };
